dda3895ac8fc7464c422610f4a8061714f3fab45
parallel-dev-ecm <emilio.cortes@parallelroom.io>
2023-04-28 22:17:11 -0600

    Added @react-three/rapier

--- src/Components/Main_scene.tsx
+++ src/Components/Main_scene.tsx
@@ -1,30 +1,22 @@
 import { Canvas } from "@react-three/fiber";
-import RoomScan from "./RoomScan";
 import * as THREE from "three";
 import StarPoints from "./StarPoints";
 import { Suspense, useRef } from "react";
 import LoadingScreen from "./LoadingScreen";
 import {
   KeyboardControls,
-  OrbitControls,
-  PerspectiveCamera,
   PointerLockControls,
   Stars,
 } from "@react-three/drei";
 import OscarTheDog from "./OscarTheDog";
 import { Physics, Debug } from "@react-three/rapier";
-import SplineRoom from "./SplineRoom";
 import Player from "./Player";
 import SpaceBoi from "./SpaceBoi";
-import BackGroundSphere from "./BackGroundSphere";
 import Ground from "./Ground";
 
 type Props = {};
 
 function Main_scene({}: Props) {
-  const roomScale: THREE.Vector3 = new THREE.Vector3(1, 1, 1);
-  const roomPosition: THREE.Vector3 = new THREE.Vector3(0, -0.5, 0);
-  const roomRotation: THREE.Vector3 = new THREE.Vector3(0, -1, 0);
   const oscarPosition: THREE.Vector3 = new THREE.Vector3(-3, 0, 3);
   const oscarRotation: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
 
@@ -42,12 +34,7 @@ function Main_scene({}: Props) {
         <Suspense fallback={<LoadingScreen />}>
           <Physics>
             <Debug />
-            <SplineRoom scale={0.05} />
-            {/* <RoomScan
-            scale={roomScale}
-            rotation={roomRotation}
-            position={roomPosition}
-          /> */}
+
             <OscarTheDog
               scale={0.006}
               position={oscarPosition}
